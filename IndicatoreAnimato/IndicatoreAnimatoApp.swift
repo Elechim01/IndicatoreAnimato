5ca8197588192e7bc5c5884c099beb29abb802d4
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-16 22:29:32 +0200

    Initial Commit

---
+++ IndicatoreAnimato/IndicatoreAnimatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndicatoreAnimatoApp.swift
+//  IndicatoreAnimato
+//
+//  Created by Michele Manniello on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct IndicatoreAnimatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
